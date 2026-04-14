b69853d3b0d33bc14ad1a95edab8e0acbac4defc
CARLOSLUZEDGAR <carlosyluzalberto@gmail.com>
2026-04-13 22:28:18 -0400

    130426_2227 continuacion de la direccion de vista de verificacion

--- routes/web.php
+++ routes/web.php
@@ -84,7 +84,7 @@ Route::post('/listarPersonal','PersonalController@ListarPersonal');
 Route::post('/selectNacionalidad','NacionalidadController@selectNacionalidad');
 Route::post('/renovarPersonal','PersonalController@RenovarPersonal');
 // Route::post('/datosLicencia','PersonalController@VerificarPersonal');
-Route::post('/verificarPersonal/{id_per_lic}','PersonalController@VerificarPersonal');
+Route::get('/verificarPersonal/{id_per_lic}','PersonalController@VerificarPersonal');
 Route::post('/listarGranUnidad','GranUnidadController@ListarGranUnidad');
 Route::post('/listarUnidad','UnidadController@ListarUnidad');
 Route::post('/listarTipoAeronave','TipoAeronaveController@ListarTipoAeronave');
