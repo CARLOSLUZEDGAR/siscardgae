12e79803f046fd8d76e97b3c38e8d662e194be51
CARLOSLUZEDGAR <carlosyluzalberto@gmail.com>
2026-04-16 15:01:13 -0400

    160426_1500 vista verificacion escalable

--- resources/views/verificacion/verificar.blade.php
+++ resources/views/verificacion/verificar.blade.php
@@ -1,124 +1,153 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <link rel="shortcut icon" href="../img/dgae_logo.png"> 
-  <title>SISCAR - DGAE</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>SISCAR - DGAE</title>
+
+<style>
+    body {
+        font-family: Arial, sans-serif;
+        font-size: 14px;
+    }
+
+    .container {
+        width: 100%;
+        padding-bottom: 1cm;
+    }
+
+    .header {
+        text-align: center;
+        font-size: 0.9rem;
+    }
+
+    .main-table {
+        width: 80%;
+        margin: 0 auto;
+        border-collapse: collapse;
+        background-image: url('../img/dgae_agua.png');
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: contain;
+    }
+
+    .main-table td {
+        padding: 4px;
+        font-size: 0.9rem;
+    }
+
+    .label {
+        text-align: right;
+        font-weight: bold;
+        width: 50%;
+    }
+
+    .value {
+        text-align: left;
+        width: 50%;
+    }
+
+    .title {
+        text-align: center;
+        font-size: 1.2rem;
+        font-weight: bold;
+    }
+
+    .image-container {
+        text-align: center;
+    }
+
+    .image-container img {
+        width: 120px;
+        height: auto;
+        border: 2px solid #142A98;
+    }
+
+    .estado-vigente {
+        color: green;
+    }
+
+    .estado-no-vigente {
+        color: red;
+    }
+</style>
+
 </head>
+
 <body>
-    <div style="padding-top: 0px; padding-bottom: 1cm;">
-        <table width="100%" style="border-collapse: collapse; border-spacing: 0; /*border: black 1px;*/ margin-bottom: 0px; " class="table table-bordered table-striped">
-            <tbody class="table-striped">
-                <tr>
-                    <td style="/*border: 1px solid #000;*/ vertical-align: top;">
-                        <div style="text-align: center; width: 334px; font-size: 10pt;">
-                            <p style="margin: 1px;">FUERZA AÉREA BOLIVIANA</p>
-                            <p style="margin: 1px;">DIRECCIÓN GENERAL DE AERONAVES DE ESTADO</p>
-                            <p style="margin: 1px;"><u><strong>BOLIVIA</strong></u></p>
-                        </div>
-                    </td>
-                </tr>  
-            </tbody>
-        </table>
+
+<div class="container">
+
+    <!-- HEADER -->
+    <div class="header">
+        <p>FUERZA AÉREA BOLIVIANA</p>
+        <p>DIRECCIÓN GENERAL DE AERONAVES DE ESTADO</p>
+        <p><strong><u>BOLIVIA</u></strong></p>
     </div>
 
-    <div>
-        <table width="80%" style="border-collapse: collapse; border-spacing: 0; margin: 0 auto 5px auto; background-image: url('../img/dgae_agua.png');
-              background-size: auto;
-              background-repeat: no-repeat;
-              background-position: center;" class="table table-bordered table-striped">
-            <tbody class="table-striped">
-                <tr>
-                    <td colspan="2" style="/*border: 1px solid #000;*/ padding: 1px; font-size: 12pt; text-align: center;">
-                        <h2>DATOS LICENCIA</h2>
-                    </td>
-                </tr>
-                <tr>
-                    <td colspan="2" style="/*border: 1px solid #000;*/ padding: 1px; text-align: center; vertical-align: middle;">
-                        <img style="width: 4cm; height: 4cm; border: 2px solid #142A98;" src="../img/personal/{{$personal->per_foto}}">
-                    </td>
-                </tr>
-                <tr>
-                    <td width="50%" style="/*border: 1px solid #000;*/ padding: 1px; font-size: 12pt; text-align: right;">
-                        <span style="margin: 1px; font-weight: bold;">GRADO:</span>
-                    </td>
-                    <td width="50%" style="/*border: 1px solid #000;*/ padding: 1px; font-size: 12pt; text-align: left;">
-                        <span style="margin: 1px;">{{$personal->abreviatura}}</span>
-                    </td>
-                </tr>
-                <tr>
-                    <td width="50%" style="/*border: 1px solid #000;*/ padding: 1px; font-size: 12pt; text-align: right; vertical-align: top;">
-                        <span style="margin: 1px; font-weight: bold;">APELLIDO(S):</span>
-                    </td>
-                    <td width="50%" style="/*border: 1px solid #000;*/ padding: 1px; font-size: 12pt; text-align: left;">
-                        <span style="margin: 1px;">{{$personal->per_paterno}} {{$personal->per_materno}}</span>
-                    </td>
-                </tr>
-                <tr>
-                    <td width="50%" style="/*border: 1px solid #000;*/ padding: 1px; font-size: 12pt; text-align: right;">
-                        <span style="margin: 1px; font-weight: bold;">NOMBRE(S):</span>
-                    </td>
-                    <td width="50%" style="/*border: 1px solid #000;*/ padding: 1px; font-size: 12pt; text-align: left;">
-                        <span style="margin: 1px;">{{$personal->per_nombre}}</span>
-                    </td>
-                </tr>
-                <tr>
-                    <td width="50%" style="/*border: 1px solid #000;*/ padding: 1px; font-size: 12pt; text-align: right;">
-                        <span style="margin: 1px; font-weight: bold;">NRO. LICENCIA:</span>
-                    </td>
-                    <td width="50%" style="/*border: 1px solid #000;*/ padding: 1px; font-size: 12pt; text-align: left;">
-                        <span style="margin: 1px;">{{$personal->per_ci}}</span>
-                    </td>
-                </tr>
-                <tr>
-                    <td width="50%" style="/*border: 1px solid #000;*/ padding: 1px; font-size: 12pt; text-align: right;">
-                        <span style="margin: 1px; font-weight: bold;">NACIONALIDAD:</span>
-                    </td>
-                    <td width="50%" style="/*border: 1px solid #000;*/ padding: 1px; font-size: 12pt; text-align: left;">
-                        <span style="margin: 1px;">{{$personal->nacionalidad}}</span>
-                    </td>
-                </tr>
-                <tr>
-                    <td width="50%" style="/*border: 1px solid #000;*/ padding: 1px; font-size: 12pt; text-align: right;">
-                        <span style="margin: 1px; font-weight: bold;">FECHA DE NAC.:</span>
-                    </td>
-                    <td width="50%" style="/*border: 1px solid #000;*/ padding: 1px; font-size: 12pt; text-align: left;">
-                        <?php
-                            $date = date_create($personal->per_fecha_nacimiento);
-                            $fechanacimiento = date_format($date,"d/m/Y");
-                        ?>
-                        <span style="margin: 1px;"><?php echo $fechanacimiento; ?></span>
-                    </td>
-                </tr>
-                <tr>
-                    <td width="50%" style="/*border: 1px solid #000;*/ padding: 1px; font-size: 12pt; text-align: right;">
-                        <span style="margin: 1px; font-weight: bold;">ESTADO:</span>
-                    </td>
-                    @if ($estado == "VIGENTE")
-                    <td width="50%" style="/*border: 1px solid #000;*/ padding: 1px; font-size: 12pt; text-align: left; color: green;">
-                        <span style="margin: 1px;">{{$estado}}</span>
-                    </td>
-                    @else
-                    <td width="50%" style="/*border: 1px solid #000;*/ padding: 1px; font-size: 12pt; text-align: left; color: red;">
-                        <span style="margin: 1px;">{{$estado}}</span>
-                    </td>
-                    @endif
-                </tr> 
-                <tr>
-                    <td width="50%" style="/*border: 1px solid #000;*/ padding: 1px; font-size: 12pt; text-align: right;">
-                        <span style="margin: 1px; font-weight: bold;">VIGENCIA:</span>
-                    </td>
-                    <td width="50%" style="/*border: 1px solid #000;*/ padding: 1px; font-size: 12pt; text-align: left;">
-                        <?php
-                            $date = date_create($personal->fecha_expiracion);
-                            $fechaexpiracion = date_format($date,"d/m/Y");
-                        ?>
-                        <span style="margin: 1px;">{{ $fechaexpiracion }}</span>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+    <!-- TABLA -->
+    <table class="main-table">
+        <tr>
+            <td colspan="2" class="title">
+                DATOS LICENCIA
+            </td>
+        </tr>
+
+        <tr>
+            <td colspan="2" class="image-container">
+                <img src="../img/personal/{{$personal->per_foto}}">
+            </td>
+        </tr>
+
+        <tr>
+            <td class="label">GRADO:</td>
+            <td class="value">{{$personal->abreviatura}}</td>
+        </tr>
+
+        <tr>
+            <td class="label">APELLIDO(S):</td>
+            <td class="value">{{$personal->per_paterno}} {{$personal->per_materno}}</td>
+        </tr>
+
+        <tr>
+            <td class="label">NOMBRE(S):</td>
+            <td class="value">{{$personal->per_nombre}}</td>
+        </tr>
+
+        <tr>
+            <td class="label">NRO. LICENCIA:</td>
+            <td class="value">{{$personal->per_ci}}</td>
+        </tr>
+
+        <tr>
+            <td class="label">NACIONALIDAD:</td>
+            <td class="value">{{$personal->nacionalidad}}</td>
+        </tr>
+
+        <tr>
+            <td class="label">FECHA DE NAC.:</td>
+            <td class="value">
+                {{ \Carbon\Carbon::parse($personal->per_fecha_nacimiento)->format('d/m/Y') }}
+            </td>
+        </tr>
+
+        <tr>
+            <td class="label">ESTADO:</td>
+            <td class="value {{ $estado == 'VIGENTE' ? 'estado-vigente' : 'estado-no-vigente' }}">
+                {{$estado}}
+            </td>
+        </tr>
+
+        <tr>
+            <td class="label">VIGENCIA:</td>
+            <td class="value">
+                {{ \Carbon\Carbon::parse($personal->fecha_expiracion)->format('d/m/Y') }}
+            </td>
+        </tr>
+
+    </table>
+
+</div>
+
 </body>
 </html>
